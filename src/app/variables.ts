dc9c5edc2714463fcdff4947ecde837d05b32180
Emilia Krzowska <>
2024-02-19 15:27:10 +0100

    feat: add variables

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,4 +1,14 @@
 
+// fetching urls variables
 export const fetchUrl = 'https://cat-fact.herokuapp.com';
 export const fetchDetails = '/facts/random?animal_type=cat&amount=1'
 export const catFetchUrl = `${fetchUrl}${fetchDetails}`;
+
+
+// text labels
+export const addToFav = "Add to favourites";
+export const removeFromFav = "Remove from favourites";
+export const factsTabTitle = "Facts";
+export const favouritesTabTitle = "Favourites";
+
+
